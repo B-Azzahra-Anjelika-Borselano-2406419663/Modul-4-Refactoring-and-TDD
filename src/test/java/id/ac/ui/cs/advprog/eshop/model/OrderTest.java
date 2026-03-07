82ebf5eac9560b492b9e379f534462edf663fd0c
anjeelika <azzahraanjelikaborselano@gmail.com>
2026-03-07 22:24:16 +0700

    [RED] Add Order model skeleton

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
@@ -76,7 +76,7 @@ public class OrderTest {
     }
 
     @Test
-    testSetStatusToInvalidStatus() {
+    void testSetStatusToInvalidStatus() {
         Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
                 this.products, 1708560000L, "Safira Sudrajat", "MEOW");
         assertThrows(IllegalArgumentException.class, () -> order.setStatus("MEOW"));
